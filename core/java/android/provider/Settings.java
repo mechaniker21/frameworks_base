1a727d78ae2dda41a27cfc871b557fd37a449e06
Alex Cruz <mazdarider23@gmail.com>
2015-12-28 13:42:21 -0500

    Power Menu animations [1/2]
    Inspired by what @maxwen did with the power menu back in 5.1.1, I wanted to make
    this optional and add an option for the top
    
    - Slide in from the bottom
    - Slide in from the top
    - Set to the default AOSP animation
    
    Signed-off-by: placiano <placiano80@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4484,6 +4484,13 @@ public final class Settings {
         public static final String ON_THE_GO_CAMERA = "on_the_go_camera";
 
         /**
+         * Whether to display set of animations when prompting the power menu
+         *
+         * @hide
+         */
+        public static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
