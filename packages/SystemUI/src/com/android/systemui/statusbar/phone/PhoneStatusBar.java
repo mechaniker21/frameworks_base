a5212c75d35d492b9230a60ebadf4340de366144
jereksel <jereksel@gmail.com>
2016-02-10 15:00:27 +0100

    ThemesTile: Items list
    AICPfy:
    - Make it work with AICP source
    
    Change-Id: I3e6f6eec883c71212ff2bea86c5634974440551f
    Signed-off-by: placiano <placiano80@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -156,6 +156,7 @@ import com.android.systemui.keyguard.KeyguardViewMediator;
 import com.android.systemui.navigation.NavigationController;
 import com.android.systemui.navigation.Navigator;
 import com.android.systemui.omni.StatusBarHeaderMachine;
+import com.android.systemui.qs.QSDetailItemsList;
 import com.android.systemui.qs.QSDragPanel;
 import com.android.systemui.recents.ScreenPinningRequest;
 import com.android.systemui.screenshot.TakeScreenshotService;
@@ -4274,8 +4275,17 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
         }
 
-        // AdapterView does not support removeAllViews so check before calling
-        if (!(parent instanceof AdapterView)) parent.removeAllViews();
+        if (parent instanceof AdapterView) {
+            //We know that when it's AdapterView it's from CM's QS detail items list
+            QSDetailItemsList.QSDetailListAdapter adapter =
+                    (QSDetailItemsList.QSDetailListAdapter) ((AdapterView) parent).getAdapter();
+
+            adapter.clear();
+            adapter.notifyDataSetInvalidated();
+        } else {
+            parent.removeAllViews();
+        }
+
     }
 
     /**
