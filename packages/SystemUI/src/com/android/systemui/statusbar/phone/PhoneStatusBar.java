daec62f4359d33d9022dd94e550cc82067d85e19
STELIX <ssspinni@gmail.com>
2016-03-14 14:25:00 +0100

    Revert "Fw_base - App sidebar: Fix Left/Right position."
    This reverts commit 14d02d9c5a5904ca6b2a057f5223b94923365ab2.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -655,14 +655,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         mWeatherTempColor, mWeatherTempSize, mWeatherTempFontStyle);
             }
 
-            int sidebarPosition = Settings.System.getInt(
-                    resolver, Settings.System.APP_SIDEBAR_POSITION, AppSidebar.SIDEBAR_POSITION_LEFT);
-            if (sidebarPosition != mSidebarPosition) {
-                mSidebarPosition = sidebarPosition;
-                removeSidebarView();
-                addSidebarView();
-            }
-
             mWeatherTempStyle = Settings.System.getIntForUser(
                     resolver, Settings.System.STATUS_BAR_WEATHER_TEMP_STYLE, 0,
                     UserHandle.USER_CURRENT);
@@ -4024,6 +4016,17 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         if (mDockBatteryController != null) {
             mDockBatteryController.setUserId(mCurrentUserId);
+        }
+    }
+
+    private void updateSettings() {
+        ContentResolver resolver = mContext.getContentResolver();
+        int sidebarPosition = Settings.System.getInt(resolver,
+                Settings.System.APP_SIDEBAR_POSITION,
+                AppSidebar.SIDEBAR_POSITION_LEFT);
+        if (sidebarPosition != mSidebarPosition) {
+            mSidebarPosition = sidebarPosition;
+            mWindowManager.updateViewLayout(mAppSidebar, getAppSidebarLayoutParams(sidebarPosition));
         }
     }
 
