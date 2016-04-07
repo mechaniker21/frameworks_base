CMRemix/android_frameworks_base

FWB: App picker navbar button (1/2)
Found some tutorial on line on how to make app picker
app dialog, so I've adopted it to the rom

Change-Id: I8165f6ad4f232921e16cc164923fa393fc731e40

Conflicts:
	packages/SystemUI/res/values/cmremix_strings.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavbarEditor.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

packages/SystemUI/src/com/android/systemui/cmremix/apppicker/AppPickerActivity.java
@@ -1,0 +1,97 @@
+package com.android.systemui.cmremix.apppicker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.systemui.R;
+
+public class AppPickerActivity extends ListActivity {
+
+    private PackageManager packageManager = null;
+    private List<ApplicationInfo> applist = null;
+    private AppPickerAdapter listadapter = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_picker);
+
+        packageManager = getPackageManager();
+
+        new LoadApplications().execute();
+    }
+
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        ApplicationInfo app = applist.get(position);
+
+        try{
+            Intent intent = packageManager.getLaunchIntentForPackage(app.packageName);
+
+            if(intent != null) {
+                startActivity(intent);
+            }
+        } catch(Exception e) {
+            Toast.makeText(AppPickerActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private List<ApplicationInfo> checkForLaunchIntent(List<ApplicationInfo> list) {
+
+        ArrayList<ApplicationInfo> appList = new ArrayList<>();
+
+        for(ApplicationInfo info : list) {
+            try{
+                if(packageManager.getLaunchIntentForPackage(info.packageName) != null) {
+                    appList.add(info);
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return appList;
+    }
+
+    private class LoadApplications extends AsyncTask<Void, Void, Void> {
+
+        private ProgressDialog progress = null;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            applist = checkForLaunchIntent(packageManager.getInstalledApplications(PackageManager.GET_META_DATA));
+
+            listadapter = new AppPickerAdapter(AppPickerActivity.this, R.layout.app_picker_list_item, applist);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            setListAdapter(listadapter);
+            progress.dismiss();
+            super.onPostExecute(result);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(AppPickerActivity.this, null, "Loading apps info...");
+            super.onPreExecute();
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/cmremix/apppicker/AppPickerAdapter.java
@@ -1,0 +1,70 @@
+package com.android.systemui.cmremix.apppicker;
+
+import java.util.List;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.systemui.R;
+
+public class AppPickerAdapter extends ArrayAdapter<ApplicationInfo>{
+
+    private List<ApplicationInfo> appList = null;
+    private Context context;
+    private PackageManager packageManager;
+
+    public AppPickerAdapter(Context context, int resource,
+                      List<ApplicationInfo> objects) {
+        super(context, resource, objects);
+
+        this.context = context;
+        this.appList = objects;
+        packageManager = context.getPackageManager();
+    }
+
+    @Override
+    public int getCount() {
+        return ((null != appList) ? appList.size() : 0);
+    }
+
+    @Override
+    public ApplicationInfo getItem(int position) {
+        return ((null != appList) ? appList.get(position) : null);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+
+        if (null == view) {
+            LayoutInflater layoutInflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = layoutInflater.inflate(R.layout.app_picker_list_item, null);
+        }
+
+        ApplicationInfo data = appList.get(position);
+
+        if (null != data) {
+            TextView appName = (TextView) view.findViewById(R.id.app_name);
+            TextView packageName = (TextView) view.findViewById(R.id.app_package);
+            ImageView iconView = (ImageView) view.findViewById(R.id.app_icon);
+
+            appName.setText(data.loadLabel(packageManager));
+            packageName.setText(data.packageName);
+            iconView.setImageDrawable(data.loadIcon(packageManager));
+        }
+        return view;
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1968,6 +1968,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 	}
     };
 
+    //App picker
+    private final View.OnClickListener mAppPickerClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            appPicker();
+        }
+    };
+
     private void awakenDreams() {
         if (mDreamManager != null) {
             try {
@@ -2144,12 +2151,19 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
     }
 
+    private void appPicker() {
+        Intent intent = new Intent(".cmremix.apppicker.AppPickerActivity");
+        startActivity(intent, true);
+    }
+
     private void prepareNavigationBarView() {
         mNavigationBarView.reorient();
 
         mNavigationBarView.setListeners(mRecentsClickListener, mRecentsPreloadOnTouchListener,
                 mLongPressBackRecentsListener, mHomeActionListener, mLongPressHomeListener,
-                mNotificationsClickListener, mNotificationsLongListener , mTorchClickListener ,mCameraClickListener, mScreenShotClickListener ,mImmersiveClickListener);
+                mNotificationsClickListener, mNotificationsLongListener, mTorchClickListener,
+                mCameraClickListener, mScreenShotClickListener, mImmersiveClickListener,
+                mAppPickerClickListener);
         mAssistManager.onConfigurationChanged();
     }
 

mechaniker21/frameworks_base

Signed-off-by: placiano <placiano80@gmail.com>

packages/SystemUI/src/com/android/systemui/emotion/apppicker/AppPickerActivity.java
@@ -1,0 +1,97 @@
+package com.android.systemui.emotion.apppicker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.systemui.R;
+
+public class AppPickerActivity extends ListActivity {
+
+    private PackageManager packageManager = null;
+    private List<ApplicationInfo> applist = null;
+    private AppPickerAdapter listadapter = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_picker);
+
+        packageManager = getPackageManager();
+
+        new LoadApplications().execute();
+    }
+
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        ApplicationInfo app = applist.get(position);
+
+        try{
+            Intent intent = packageManager.getLaunchIntentForPackage(app.packageName);
+
+            if(intent != null) {
+                startActivity(intent);
+            }
+        } catch(Exception e) {
+            Toast.makeText(AppPickerActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private List<ApplicationInfo> checkForLaunchIntent(List<ApplicationInfo> list) {
+
+        ArrayList<ApplicationInfo> appList = new ArrayList<>();
+
+        for(ApplicationInfo info : list) {
+            try{
+                if(packageManager.getLaunchIntentForPackage(info.packageName) != null) {
+                    appList.add(info);
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return appList;
+    }
+
+    private class LoadApplications extends AsyncTask<Void, Void, Void> {
+
+        private ProgressDialog progress = null;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            applist = checkForLaunchIntent(packageManager.getInstalledApplications(PackageManager.GET_META_DATA));
+
+            listadapter = new AppPickerAdapter(AppPickerActivity.this, R.layout.app_picker_list_item, applist);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            setListAdapter(listadapter);
+            progress.dismiss();
+            super.onPostExecute(result);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(AppPickerActivity.this, null, "Loading apps info...");
+            super.onPreExecute();
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/emotion/apppicker/AppPickerAdapter.java
@@ -1,0 +1,70 @@
+package com.android.systemui.emotion.apppicker;
+
+import java.util.List;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.systemui.R;
+
+public class AppPickerAdapter extends ArrayAdapter<ApplicationInfo>{
+
+    private List<ApplicationInfo> appList = null;
+    private Context context;
+    private PackageManager packageManager;
+
+    public AppPickerAdapter(Context context, int resource,
+                      List<ApplicationInfo> objects) {
+        super(context, resource, objects);
+
+        this.context = context;
+        this.appList = objects;
+        packageManager = context.getPackageManager();
+    }
+
+    @Override
+    public int getCount() {
+        return ((null != appList) ? appList.size() : 0);
+    }
+
+    @Override
+    public ApplicationInfo getItem(int position) {
+        return ((null != appList) ? appList.get(position) : null);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+
+        if (null == view) {
+            LayoutInflater layoutInflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = layoutInflater.inflate(R.layout.app_picker_list_item, null);
+        }
+
+        ApplicationInfo data = appList.get(position);
+
+        if (null != data) {
+            TextView appName = (TextView) view.findViewById(R.id.app_name);
+            TextView packageName = (TextView) view.findViewById(R.id.app_package);
+            ImageView iconView = (ImageView) view.findViewById(R.id.app_icon);
+
+            appName.setText(data.loadLabel(packageManager));
+            packageName.setText(data.packageName);
+            iconView.setImageDrawable(data.loadIcon(packageManager));
+        }
+        return view;
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1905,6 +1905,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 	}
     };
 
+    //App picker
+    private final View.OnClickListener mAppPickerClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            appPicker();
+        }
+    };
+
     private void awakenDreams() {
         if (mDreamManager != null) {
             try {
@@ -2081,12 +2088,19 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
     }
 
+    private void appPicker() {
+        Intent intent = new Intent(".emotion.apppicker.AppPickerActivity");
+        startActivity(intent, true);
+    }
+
     private void prepareNavigationBarView() {
         mNavigationBarView.reorient();
 
         mNavigationBarView.setListeners(mRecentsClickListener, mRecentsPreloadOnTouchListener,
                 mLongPressBackRecentsListener, mHomeActionListener, mLongPressHomeListener,
-                mNotificationsClickListener, mNotificationsLongListener , mTorchClickListener ,mCameraClickListener, mScreenShotClickListener ,mImmersiveClickListener);
+                mNotificationsClickListener, mNotificationsLongListener, mTorchClickListener,
+                mCameraClickListener, mScreenShotClickListener, mImmersiveClickListener,
+                mAppPickerClickListener);
         mAssistManager.onConfigurationChanged();
     }
 