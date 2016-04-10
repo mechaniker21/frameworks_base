87ce100cc15e7a96e13722dcf38d5b790f7f0ae2
placiano <placiano80@gmail.com>
2016-04-11 00:39:04 +0200

    Revert "Fw_base - SlimDim: Use semaphore to cancel ontouch callback"
    This reverts commit 67e8a820862fec188554af838d7474b9d3348458.
    
    Signed-off-by: placiano <placiano80@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -112,7 +112,6 @@ public class NavigationBarView extends LinearLayout {
     private boolean mDimNavButtonsAnimate;
     private int mDimNavButtonsAnimateDuration;
     private boolean mDimNavButtonsTouchAnywhere;
-    private boolean mIsHandlerCallbackActive = false;
 
     private NavigationBarViewTaskSwitchHelper mTaskSwitchHelper;
     private DeadZone mDeadZone;
@@ -225,10 +224,7 @@ public class NavigationBarView extends LinearLayout {
     };
 
     public void onNavButtonTouched() {
-        if (mIsHandlerCallbackActive) {
-            mHandler.removeCallbacks(mNavButtonDimmer);
-            mIsHandlerCallbackActive = false;
-        }
+        mHandler.removeCallbacks(mNavButtonDimmer);
         final boolean keyguardProbablyEnabled =
                 (mDisabledFlags & View.STATUS_BAR_DISABLE_HOME) != 0;
         if (getNavButtons() != null) {
@@ -241,7 +237,6 @@ public class NavigationBarView extends LinearLayout {
             }
             if (mDimNavButtons && !keyguardProbablyEnabled) {
                 mHandler.postDelayed(mNavButtonDimmer, mDimNavButtonsTimeout);
-                mIsHandlerCallbackActive = true;
             }
         }
     }
@@ -1073,7 +1068,6 @@ public class NavigationBarView extends LinearLayout {
     private Runnable mNavButtonDimmer = new Runnable() {
         @Override
         public void run() {
-            mIsHandlerCallbackActive = false;
             if (getNavButtons() != null && mIsDim == false) {
                 mIsDim = true;
                 if (mDimNavButtonsAnimate) {
