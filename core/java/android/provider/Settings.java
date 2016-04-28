dad755396922f35e96582ff9e34da523f4f4027c
Varun Date <date.varun123@gmail.com>
2016-04-28 22:18:48 +0530

    Pulse Visualizer Sanity Level [1/3]
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Signed-off-by: placiano <placiano80@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3932,6 +3932,12 @@ public final class Settings {
          */
         public static final String PULSE_EMPTY_BLOCK_SIZE = "pulse_empty_block_size";
         
+         /**
+         * Pulse Fudge Factor
+         * @hide
+         */
+        public static final String PULSE_CUSTOM_FUDGE_FACTOR = "pulse_custom_fudge_factor";
+        
         /**
          * Whether to display app circle sidebar
          * @hide
