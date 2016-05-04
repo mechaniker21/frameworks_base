d762603906353aaa36d5b41ab78218bd6bb3438a
Roman Birg <roman@cyngn.com>
2016-05-04 13:51:55 -0700

    SystemUI: implement properly saving tile locations
    Use new persisted key which does not use the tiles' ID (which changes
    usually across reboots with every tile). When the Custom tile service
    comes up, it will update the placeholder tiles created with their real
    values and the order is preserved properly.
    
    Ticket: CYNGNOS-2530
    
    Change-Id: I5e75c739553969f1d92c249f521a476259fe0cd6
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -384,7 +384,12 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("navbar")) return new NavBarTile(this);
         else if (tileSpec.equals("float_mode")) return new FloatingWindowsTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
-        else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
+        else if (TextUtils.split(tileSpec, "\\|").length == 3) {
+            /** restores placeholder for
+             * {@link cyanogenmod.app.StatusBarPanelCustomTile#persistableKey()} **/
+            return new CustomQSTile(this, tileSpec);
+        } else
+            throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
 
     protected List<String> loadTileSpecs(String tileList) {
@@ -522,8 +527,8 @@ public class QSTileHost implements QSTile.Host, Tunable {
 
     void updateCustomTile(StatusBarPanelCustomTile sbc) {
         synchronized (mTiles) {
-            if (mTiles.containsKey(sbc.getKey())) {
-                QSTile<?> tile = mTiles.get(sbc.getKey());
+            if (mTiles.containsKey(sbc.persistableKey())) {
+                QSTile<?> tile = mTiles.get(sbc.persistableKey());
                 if (tile instanceof CustomQSTile) {
                     CustomQSTile qsTile = (CustomQSTile) tile;
                     qsTile.update(sbc);
@@ -535,8 +540,8 @@ public class QSTileHost implements QSTile.Host, Tunable {
     void addCustomTile(StatusBarPanelCustomTile sbc) {
         synchronized (mTiles) {
             mCustomTileData.add(new CustomTileData.Entry(sbc));
-            mTileSpecs.add(sbc.getKey());
-            mTiles.put(sbc.getKey(), new CustomQSTile(this, sbc));
+            mTileSpecs.add(sbc.persistableKey());
+            mTiles.put(sbc.persistableKey(), new CustomQSTile(this, sbc));
             if (mCallback != null) {
                 mCallback.onTilesChanged();
             }
