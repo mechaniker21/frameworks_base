d433d1d4ea795377732782734fe66dfc0f1d4098
Danesh M <danesh@cyngn.com>
2016-05-08 20:16:33 -0700

    Keyguard : Ensure we expand onIconClicked
    Not doing so shows visible clipping when the notification
    panel is expanded mainly due to the limited height of the bar.
    
    Change-Id: I50a35b186b3efb8a0e4ba0ae18ff915aebe236c6
    Signed-off-by: placiano <placiano80@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2443,6 +2443,7 @@ public class NotificationPanelView extends PanelView implements
             return;
         }
         mHintAnimationRunning = true;
+        mKeyguardBottomArea.expand(true);
         mAfforanceHelper.startHintAnimation(rightIcon, new Runnable() {
             @Override
             public void run() {
