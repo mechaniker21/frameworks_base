036a76040eb5f6be8e2ca2f6237c2e31c261425e
d34d <clark@cyngn.com>
2016-05-16 16:29:33 -0700

    LLS: Don't change bar state when changing LLS focus
    State changes are taken care of elsewhere so don't change the state
    when unfocusing the LLS.
    
    Change-Id: Ie4d14a840cfc2f29815bd6d185caf920134b6d21
    TICKET: FEIJ-531
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -5127,7 +5127,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     protected void unfocusKeyguardExternalView() {
-        setBarState(StatusBarState.KEYGUARD);
         mStatusBarKeyguardViewManager.setKeyguardExternalViewFocus(false);
     }
 
